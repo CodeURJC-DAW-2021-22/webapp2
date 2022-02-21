c3ca7cd8b11c268c409dba1ef23b5e4252fc5288
Fcardiperez <fran.cardi.31@gmail.com>
2022-02-21 13:13:54 +0100

    sping  boot

---
+++ backend/src/test/java/es/codeurjc/flyventas/FlyventasApplicationTests.java
@@ -0,0 +1,13 @@
+package es.codeurjc.flyventas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlyventasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

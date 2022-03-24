e8cce712c8fc6c2ee2ebb3b2350bfb636baaeacf
samuelrusu3 <s.rusu.2019@gmail.com>
2022-03-24 20:45:24 +0100

    UserREST & SecurityREST

--- backend/src/main/java/es/codeurjc/Flyventas/controller/UserRestController.java
+++ backend/src/main/java/es/codeurjc/Flyventas/controller/UserRestController.java
@@ -1,4 +1,36 @@
 package es.codeurjc.Flyventas.controller;
 
+import java.security.Principal;
+
+import javax.servlet.http.HttpServletRequest;
+
+import es.codeurjc.Flyventas.model.User;
+import es.codeurjc.Flyventas.repository.UserRepository;
+import es.codeurjc.Flyventas.services.UserServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/api/users")
 public class UserRestController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private UserServices userService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getProfile(@PathVariable long id) {
+
+        User user = userService.findUserById(id).orElseThrow();
+
+        if(user != null) {
+            return ResponseEntity.ok(userRepository.findById(id).orElseThrow());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }

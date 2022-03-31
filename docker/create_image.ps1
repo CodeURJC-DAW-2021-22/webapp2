5c071f67eed885d0ddedeedd882104698e986e43
migueelruiiz <81260719+migueelruiiz@users.noreply.github.com>
2022-03-31 17:32:20 +0200

    Update create_image.ps1

--- docker/create_image.ps1
+++ docker/create_image.ps1
@@ -1,5 +1,5 @@
 #Creamos la imagen a partir del fichero Dockerfile
-docker build -t njhvfm014 .
+docker build -t njhvfm014/codeurjc-daw-2021-22-webapp2 .
 
 #Pusheamos la imagen creada a la cuenta de dockerHub
-docker push njhvfm014
+docker push njhvfm014/codeurjc-daw-2021-22-webapp2

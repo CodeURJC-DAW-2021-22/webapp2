6c1006eb607123965f6dd956d9cf5441ba14622f
migueelruiiz <81260719+migueelruiiz@users.noreply.github.com>
2022-03-31 13:12:03 +0200

    Update create_image.ps1

--- docker/create_image.ps1
+++ docker/create_image.ps1
@@ -1,5 +1,5 @@
 #Creamos la imagen a partir del fichero Dockerfile
-docker build -t njhvfm014
+docker build -t njhvfm014 .
 
 #Pusheamos la imagen creada a la cuenta de dockerHub
 docker push njhvfm014

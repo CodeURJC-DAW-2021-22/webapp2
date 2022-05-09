10b72566a0eaad038e29de652552636f427f8bb2
rukusxd <fran.cardi.31@gmail.com>
2022-05-10 01:15:39 +0200

    docker y despliegue

--- backend/src/main/java/es/codeurjc/Flyventas/controller/SPAController.java
+++ backend/src/main/java/es/codeurjc/Flyventas/controller/SPAController.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class SPAController {
-    @GetMapping({"/new/**/{path:[^\\.]*}", "/{path:new[^\\.]*}"})
+    @GetMapping({"/new/{path:[^\\.]*}", "/{path:new[^\\.]*}"})
     public String redirect() {
-        return "forward:/new/";
+        return "forward:/new/index.html";
     }
 }
--- docker/create_image.ps1
+++ docker/create_image.ps1
@@ -1,8 +1,3 @@
-cd ../frontend/FlyventasAngular
-npm install
-ng build --prod --base-href="/new/"
 rm -r ../../backend/src/main/resources/static/new
-cp -r dist/flyventas-angular/* ../../backend/src/main/resources/static/new
-cd ../../docker
-docker build -f Dockerfile -t registry.heroku.com/codeurjc-daw-2021-22-webapp2/web ..
-docker push registry.heroku.com/codeurjc-daw-2021-22-webapp2/web 
+docker build -f Dockerfile -t registry.heroku.com/codeurjc-daw-2021-22-webapp2/web .. 
+

f589cdddaebda2b4c4efdb27f59b52bb009c25db
Fcardiperez <fran.cardi.31@gmail.com>
2022-05-09 15:31:58 +0200

    bugs docker

--- docker/create_image.ps1
+++ docker/create_image.ps1
@@ -2,8 +2,8 @@
 cd ../frontend/FlyventasAngular
 npm install
 npm build --prod --base-href="/new/"
-rm -r ../backend/src/main/resources/public/new/*
-cp -r dist/flyventas-angular/* ../backend/src/main/resources/public/new
+rm -r ../backend/src/main/resources/static/new/*
+cp -r dist/flyventas-angular/* ../backend/src/main/resources/static/new
 cd ../../docker
 docker build -f Dockerfile -t registry.heroku.com/codeurjc-daw-2021-22-webapp2/web ../backend
 docker push registry.heroku.com/codeurjc-daw-2021-22-webapp2/web 

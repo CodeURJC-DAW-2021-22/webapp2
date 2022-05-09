f5e0fd8ebf2fbf2ebd89876ba46278a865f85a2e
rukusxd <fran.cardi.31@gmail.com>
2022-05-09 10:37:15 +0200

    docker

--- docker/create_image.ps1
+++ docker/create_image.ps1
@@ -1,10 +1,10 @@
 #Creamos la imagen a partir del fichero Dockerfile y la Pusheamos la imagen creada a la cuenta de dockerHub
 cd ../frontend/FlyventasAngular
 npm install
-npm run build --prod --base-href="/new/"
+npm build --prod --base-href="/new/"
 cp -r dist/flyventas-angular/* ../backend/src/main/resources/public/new
 cd ../../docker
-docker build -f Dockerfile -t registry.heroku.com/codeurjc-daw-2021-22-webapp2/web .. ../backend
+docker build -f Dockerfile -t registry.heroku.com/codeurjc-daw-2021-22-webapp2/web ../backend
 docker push registry.heroku.com/codeurjc-daw-2021-22-webapp2/web 
 
 

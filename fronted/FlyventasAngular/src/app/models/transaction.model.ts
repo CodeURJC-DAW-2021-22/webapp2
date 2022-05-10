4cd81f6dcb7e39a5ac7c400feccfbf2eab62e3a2
rukusxd <fran.cardi.31@gmail.com>
2022-05-10 07:22:49 +0200

    bugs final

--- fronted/FlyventasAngular/src/app/models/transaction.model.ts
+++ fronted/FlyventasAngular/src/app/models/transaction.model.ts
@@ -5,7 +5,7 @@ export interface Transaction {
     id?: number;
     date: String;
     price: number;
-    product: Product;
-    buyer?: User;
-    seller?: User;
+    product_id: {id?: number,title:string,description: string, category: string, price: number, isSold: boolean, image: boolean,user?: User;};
+    seller_id?: { id?: number; name: string; surname: string; email: string; address: string; roles: string[]; encodedPassword:string; categoria1: string; categoria2: string; categoria3: string; products: Product[] };
+    buyer_id?: { id?: number; name: string; surname: string; email: string; address: string; roles: string[]; encodedPassword:string; categoria1: string; categoria2: string; categoria3: string; products: Product[] };
 }
